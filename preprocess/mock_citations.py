010fe49412a21c5a1a67f4da36bc6ce955f3e9db
Furkan Kocak <kocakberatfurkan@gmail.com>
2024-04-10 17:10:26 +0200

    part b Completed

--- preprocess/mock_citations.py
+++ preprocess/mock_citations.py
@@ -18,19 +18,34 @@ paper_ids = [paper['paperId'] for paper in raw_paper_data]
 
 #create 10 mock citations for each paper in the paper_ids list to another paper in the paper_ids list
 reference_details = []
+paper_references= []
+paper_limits = []
+#fill paper_references with key value pairs. keys are paper_ids and values are 0
+for paper in paper_ids:
+    rand = random.randint(5, 12)
+    paper_limits.append({paper: rand})
+
+for paper in paper_ids:
+    paper_references.append({paper: 0})
+    
 for paper in paper_ids:
     for i in range(1, 9):
         paper_new = paper + "-" + str(i)
 
         #create random number between 20 and 50
-        rand= random.randint(20, 50)
+        rand= random.randint(1, 3)
         for i in range(rand):
             referenceId = random.choice(paper_ids)
             if paper_new != referenceId:
+                if paper_references[paper_ids.index(paper)][paper] >= paper_limits[paper_ids.index(paper)][paper]:
+                    break
                 #create fake year between 2017 and 2021
                 year = faker.date_time_between(start_date='-5y', end_date='now').year
             
                 reference_details.append({'paperId': paper_new, 'referenceId': referenceId, 'year': year})
+                paper_references[paper_ids.index(paper)][paper] += 1
+               
+
             #paper cannot be a reference to itself
 
 
